57220d0b0db9d0b93b86322d8836b3b4d03d1556
Lucas <lucaswoodyard@yahoo.com>
2022-08-21 14:59:06 -0500

    commit tree 1930eef3e2763b8b8084121dfaac02c66954f0be

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+function reverse(word) {
+  // const wordArray = word.split("")
+  // const reverseWordArray = wordArray.reverse()
+  // const reverseWord = reverseWordArray.join("")
+  //   return reverseWord; 
+
+    return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  const reverseWord = reverse(word);
+      return word === reverseWord;
+ }
 
 /* 
   Add your pseudocode here
@@ -16,10 +27,21 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("flolf"))
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("gololgoglolog"))
+
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("pneumonoultramicroscopicsilicovolcanoconiosis"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("poster"));
 }
 
 module.exports = isPalindrome;
